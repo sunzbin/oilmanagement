4d943d68b7dc5e5e1ea8185b4e3f5dda60f38f2e
liangyingn <liangyingn@163.com>
2018-05-14 13:42:36 +0800

    修改批量保存实现

--- jeefw/src/com/jeefw/dao/equipment/impl/LoginLabelDaoImpl.java
+++ jeefw/src/com/jeefw/dao/equipment/impl/LoginLabelDaoImpl.java
@@ -2,9 +2,11 @@ package com.jeefw.dao.equipment.impl;
 
 import java.util.List;
 
+import org.hibernate.Session;
 import org.springframework.stereotype.Repository;
 
 import com.jeefw.dao.equipment.LoginLabelDao;
+import com.jeefw.model.equipment.AxisLabel;
 import com.jeefw.model.equipment.LoginLabel;
 
 import core.dao.BaseDao;
@@ -24,8 +26,28 @@ public class LoginLabelDaoImpl extends BaseDao<LoginLabel> implements LoginLabel
 
 	@Override
 	public void batchSaveAxisLabel(List<LoginLabel> loginLabels) {
-		
+		if(null != loginLabels && loginLabels.size()>0) {
+			Session  session = this.getSessionFactory().openSession();
+			try {
+				session.beginTransaction();//开启事务
+				LoginLabel axisLabel = null;
+				for (int i = 0; i < loginLabels.size(); i++) {  
+					axisLabel = (LoginLabel) loginLabels.get(i); 
+		            session.save(axisLabel); // 
+		            // 批插入的对象立即写入数据库并释放内存  
+		            if (i %30 == 0) {  
+		                session.flush();  
+		                session.clear();  
+		            }  
+		        }
+				session.getTransaction().commit(); // 提交事物  
+			}catch (Exception e) {
+				e.printStackTrace();
+				session.getTransaction().rollback();//回滚事务
+			}finally {
+				session.close();
+			}
+		}
 	}
-
 	
 }
